72d4f03a6e192ce52c14a2c43044031f71817dc9
exit0508 <17ad060@ms.dendai.ac.jp>
2024-06-30 22:15:38 +0900

    Add link to About

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,18 @@
+import { Link } from "wouter";
 import Title from "./Title";
+import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
-    <section className="mb-20">
+    <section className="mb-20 mt-5">
       <div className="container">
         <p className="font-UDGothic font-bold text-3xl mb-3">こんにちは！</p>
         <Title />
+        <Link href="/about">
+          <Button className="mt-5 hover:bg-black/60 active:bg-secondary">
+            Know more?
+          </Button>
+        </Link>
       </div>
     </section>
   );
