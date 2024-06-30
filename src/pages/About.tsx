ba9216ccc51bc707e412816fa93c9f25ec4e7aa5
exit0508 <17ad060@ms.dendai.ac.jp>
2024-07-01 02:12:39 +0900

    Add list rendering

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,54 +1,143 @@
 import Title from "../components/Title";
 
+type skillsProps = skillCategories[];
+
+type skillCategories = {
+  category: string;
+  Items: SkillItem[];
+};
+
+type SkillItem = {
+  skillName: string;
+  icon?: JSX.Element;
+};
+
+const skills: skillsProps = [
+  {
+    category: "As a Graphic Designer",
+    Items: [
+      {
+        skillName: "Clip Studio Paint",
+      },
+      {
+        skillName: "Other paint softwares",
+      },
+      {
+        skillName: "HTML",
+      },
+      {
+        skillName: "CSS",
+      },
+      {
+        skillName: "Tailwind CSS",
+      },
+      {
+        skillName: "Illustration",
+      },
+    ],
+  },
+  {
+    category: "As an Interaction Designer",
+    Items: [
+      {
+        skillName: "Javascript",
+      },
+      {
+        skillName: "Typescript",
+      },
+      {
+        skillName: "React",
+      },
+      { skillName: "Git" },
+      { skillName: "Github" },
+      { skillName: "Gitlab" },
+    ],
+  },
+  {
+    category: "Others",
+    Items: [
+      {
+        skillName: "Japanese (Native level)",
+      },
+      {
+        skillName: "English (Conversational level)",
+      },
+      {
+        skillName: "User interview",
+      },
+      {
+        skillName: "Discussion facilitater",
+      },
+    ],
+  },
+];
+
+const SkillList: React.FC<{ skills: skillsProps }> = ({ skills }) => {
+  return (
+    <div className="flex flex-wrap gap-4">
+      {skills.map((category, index) => (
+        <div key={index} className="min-w-0 flex-1">
+          <h2 className="font-bold">{category.category}</h2>
+          <ul>
+            {category.Items.map((item, idx) => (
+              <li key={idx}>
+                {item.icon && item.icon}
+                {item.skillName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const SkillItem = ({ item }: { item: SkillItem }) => (
+  <div>
+    {item.icon && <span>{item.icon}</span>}
+    <span>{item.skillName}</span>
+  </div>
+);
+
 const About = () => {
   return (
-    <>
+    <div className="">
       <Title />
-      <div className="mt-10">
-        <div>
-          <h1 className="text-2xl mb-5">Motivation</h1>
-          <h2 className="text-xl mb-2">Empathy</h2>
-          <p className="text-xl mb-10">
-            Emphasis is the most essential part of my motivation. If I found
+      <div className="mt-10 mb-5">
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl font-bold md:mb-5 sm:mb-2">Motivation</h1>
+          <h2 className="sm:text-xl text-2xl mb-2 italic">
+            Plureverse and Empathy
+          </h2>
+          <p className="sm:text-base md:text-xl mb-10">
+            Emphasis is the most essential part of my motivation. When I found
             "ontological design" to be a first, it makes me much more certain of
             it. There are many taken for granted for invisible existence. I
-            would like to interact with them with empathy and corperate them to
-            make a more comfortable society. I will not stick to "specific
-            means". I'll always give priority to solving problems. However,
-            because of my academic background, I have an interest in
-            human-digital interaction. I have broad interests in interface
-            design, so it would be great if I could help you with my design and
-            front-end development skills!
+            would like to interact with them with empathy and corperate their
+            plureversal perspectives to make a more comfortable society. I will
+            not stick to "specific means". I'll always give priority to solving
+            problems. However, because of my academic background, I have an
+            interest in human-digital interaction. I have broad interests in
+            interface design, so it would be great if I could help you with my
+            design and front-end development skills!
           </p>
         </div>
-        <div>
-          <h1 className="text-2xl mb-5">Skills</h1>
-          <p className="mt-3">
-            I own basic skills using below them ,and am an autonomous
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl font-bold md:mb-5 sm:mb-2">Skills</h1>
+          <p className="mt-3 sm:text-base md:text-xl md:mb-5 sm:mb-3">
+            I own basic skills using below them, and am an autonomous
             self-learner.
           </p>
-          <h3 className="mt-2">Tech skillset</h3>
-          <ul>
-            <li>Figma / Adobe XD</li>
-            <li>CSS / HTML</li>
-            <li>Typescript / Javascript</li>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>Git</li>
-          </ul>
-          <h3 className="mt-2">Others</h3>
-          <ul>
-            <li>Japanese (NativeLevel)</li>
-            <li>English (Conversational Level)</li>
-            <li>Facilitate discussions</li>
-            <li>Qualitative analysis for user interface and experience</li>
-          </ul>
+          <SkillList skills={skills} />
+          <div className="mt-3 font-light">
+            ...Ask me if you wanna know other skills!
+          </div>
         </div>
         {/* <div>
           <h1 className="text-2xl mb-10">Timeline</h1>
         </div> */}
       </div>
-    </>
+    </div>
   );
 };
 
