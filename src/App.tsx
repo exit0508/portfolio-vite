ced6531ebd9484ffb05b2a0ffdceb91403e9af9b
exit0508 <17ad060@ms.dendai.ac.jp>
2024-06-30 21:55:14 +0900

    Add header and fix style

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,21 @@ function App() {
 
   return (
     <div className="font-Roboto w-full min-h-screen bg-primary dark:bg-gray-950 text-black dark:text-white">
-      <div className="flex justify-center md:justify-start md:ml-3">
-        <Header />
-      </div>
+      {loading ? (
+        <h1>Error</h1>
+      ) : (
+        <>
+          <div className="sticky top-3 translate-x-6">
+            <Header />
+          </div>
 
-      <div className="container px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 top-10">
-        {loading ? (
-          <h1>Error</h1>
-        ) : (
-          <>
-            <Switch>
-              <Route path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/projects" component={Projects} />
-            </Switch>
-          </>
-        )}
-      </div>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+          </Switch>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,35 @@
 import { Link } from "wouter";
-import { AlignJustify } from "lucide-react";
-import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header className="fixed top-3 rounded-full bg-white bg-white/90 backdrop-blur-lg">
-      <div className="container px-10 py-3 flex items-center gap-8">
-        <div className="text-xl">
-          <Link href="/">
-            <a className="text-secondary font-bold hover:text-primaryHover dark:text-primaryHoverDark">
-              Yurina Deguchi
-            </a>
-          </Link>
-        </div>
-        <div className="text-xl flex flex-1 items-center justify-end md:justify-between">
-          <nav aria-label="Global" className="hidden md:block">
-            <ul className="flex items-center gap-6 text-xl">
+    <header>
+      <div className="lg:w-1/4 w- md:w-2/5 sm: w-2/3 rounded-full bg-white bg-white/90 backdrop-blur-lg">
+        <div className="flex items-center justify-around py-3 px-5">
+          <div>
+            <ul className="flex items-center space-between gap-4">
+              <li>
+                <Link href="/">
+                  <a className="text-black font-bold hover:text-black/60 active:text-secondary dark:text-primaryHoverDark">
+                    Yurina Deguchi
+                  </a>
+                </Link>
+              </li>
               <li>
                 <Link href="/about">
-                  <a className="text-black hover:text-primaryHover dark:text-primaryHoverDark">
+                  <a className="text-black hover:text-black/60 active:text-secondary dark:text-primaryHoverDark">
                     About
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/projects">
-                  <a className="text-black hover:text-primaryHover dark:text-primaryHoverDark">
+                  <a className="text-black hover:text-black/60 active:text-secondary dark:text-primaryHoverDark">
                     Projects
                   </a>
                 </Link>
               </li>
             </ul>
-          </nav>
-        </div>
-        <div>
-          <Button className="block rounded p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-            <AlignJustify />
-          </Button>
+          </div>
         </div>
       </div>
     </header>
