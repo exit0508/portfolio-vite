0f353414aefba94dd095a8d6d6bae9cc435555f0
exit0508 <17ad060@ms.dendai.ac.jp>
2024-07-01 14:27:24 +0900

    Display thumbnail on card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,21 +4,28 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
+  CardContent,
 } from "@/components/ui/card";
+import { ExternalLink } from "lucide-react";
 import { ProjectProps } from "../utils/projectContext";
 
 const ProjectCard = ({ project }: { project: ProjectProps }) => {
   return (
     <div className="pb-5">
-      <Card key={project.id}>
+      <Card key={project.id} className="w-[300px]">
         <CardHeader>
-          <CardTitle>{project.title}</CardTitle>
+          <CardTitle className="text-lg">{project.title}</CardTitle>
           <CardDescription>{project.projectDate}</CardDescription>
         </CardHeader>
-        {/* <CardContent></CardContent> */}
+        <CardContent>
+          <img src={project.thumbnail} alt="thumbnail" />
+        </CardContent>
         <CardFooter>
           <a href={project.publicLink}>
-            <p>Read more</p>
+            <div className="flex items-center">
+              Read more{" "}
+              <ExternalLink className="h-5 ml-2 mb-1 stroke-black/60" />
+            </div>
           </a>
         </CardFooter>
       </Card>
