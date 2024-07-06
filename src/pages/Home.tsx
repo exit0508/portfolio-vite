0e8885948e7e5a360c4a9086b4571fe3a779f9dd
exit0508 <17ad060@ms.dendai.ac.jp>
2024-07-06 23:31:17 +0900

    Remove unused components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,9 @@
 import Hero from "../components/Hero";
-import CardList from "../components/CardList";
-import { useProjects } from "@/utils/projectContext";
-import { use, ProjectProps, fetchData } from "../utils/projectData";
-import ProjectTemp from "@/components/ProjectTemp";
-import { startTransition, Suspense, useState } from "react";
+import { Suspense } from "react";
 import RecentPostsSection from "@/components/RecentPostsSection";
 import LoadingCards from "@/components/LoadingCards";
 
 const Home = () => {
-  //const { projects } = useProjects();
-
   return (
     <div className="container">
       <div className="pt-1">
