2569a0df2edf183e9325b8b7c63cad15c688f339
exit0508 <17ad060@ms.dendai.ac.jp>
2024-07-06 22:31:13 +0900

    Edit dev api to prod api

--- src/utils/projectData.tsx
+++ src/utils/projectData.tsx
@@ -1,85 +1,85 @@
-const dummyData = [
-  {
-    id: "1",
-    title: "Responsive Web Design",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2023-01-15",
-    tags: ["HTML", "CSS", "Responsive Design"],
-    publicLink: "https://example.com/project1",
-  },
-  {
-    id: "2",
-    title: "E-commerce Platform",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2022-12-05",
-    tags: ["React", "Node.js", "E-commerce"],
-    publicLink: "https://example.com/project2",
-  },
-  {
-    id: "3",
-    title: "Mobile App Development",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2023-02-20",
-    tags: ["React Native", "Mobile App"],
-    publicLink: "https://example.com/project3",
-  },
-  {
-    id: "4",
-    title: "Portfolio Website",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2021-11-18",
-    tags: ["HTML", "CSS", "JavaScript"],
-    publicLink: "https://example.com/project4",
-  },
-  {
-    id: "5",
-    title: "Machine Learning Model",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2023-03-25",
-    tags: ["Python", "Machine Learning"],
-    publicLink: "https://example.com/project5",
-  },
-  {
-    id: "6",
-    title: "Game Development",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2022-09-14",
-    tags: ["Unity", "C#"],
-    publicLink: "https://example.com/project6",
-  },
-  {
-    id: "7",
-    title: "Weather App",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2023-05-10",
-    tags: ["React", "API"],
-    publicLink: "https://example.com/project7",
-  },
-  {
-    id: "8",
-    title: "Blog Platform",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2022-10-08",
-    tags: ["Node.js", "Express", "MongoDB"],
-    publicLink: "https://example.com/project8",
-  },
-  {
-    id: "9",
-    title: "Social Media Integration",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2023-06-15",
-    tags: ["JavaScript", "API", "Social Media"],
-    publicLink: "https://example.com/project9",
-  },
-  {
-    id: "10",
-    title: "AI Chatbot",
-    thumbnail: "https://placehold.jp/800x600.png",
-    projectDate: "2023-04-22",
-    tags: ["Python", "AI", "NLP"],
-    publicLink: "https://example.com/project10",
-  },
-];
+// const dummyData = [
+//   {
+//     id: "1",
+//     title: "Responsive Web Design",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2023-01-15",
+//     tags: ["HTML", "CSS", "Responsive Design"],
+//     publicLink: "https://example.com/project1",
+//   },
+//   {
+//     id: "2",
+//     title: "E-commerce Platform",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2022-12-05",
+//     tags: ["React", "Node.js", "E-commerce"],
+//     publicLink: "https://example.com/project2",
+//   },
+//   {
+//     id: "3",
+//     title: "Mobile App Development",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2023-02-20",
+//     tags: ["React Native", "Mobile App"],
+//     publicLink: "https://example.com/project3",
+//   },
+//   {
+//     id: "4",
+//     title: "Portfolio Website",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2021-11-18",
+//     tags: ["HTML", "CSS", "JavaScript"],
+//     publicLink: "https://example.com/project4",
+//   },
+//   {
+//     id: "5",
+//     title: "Machine Learning Model",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2023-03-25",
+//     tags: ["Python", "Machine Learning"],
+//     publicLink: "https://example.com/project5",
+//   },
+//   {
+//     id: "6",
+//     title: "Game Development",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2022-09-14",
+//     tags: ["Unity", "C#"],
+//     publicLink: "https://example.com/project6",
+//   },
+//   {
+//     id: "7",
+//     title: "Weather App",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2023-05-10",
+//     tags: ["React", "API"],
+//     publicLink: "https://example.com/project7",
+//   },
+//   {
+//     id: "8",
+//     title: "Blog Platform",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2022-10-08",
+//     tags: ["Node.js", "Express", "MongoDB"],
+//     publicLink: "https://example.com/project8",
+//   },
+//   {
+//     id: "9",
+//     title: "Social Media Integration",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2023-06-15",
+//     tags: ["JavaScript", "API", "Social Media"],
+//     publicLink: "https://example.com/project9",
+//   },
+//   {
+//     id: "10",
+//     title: "AI Chatbot",
+//     thumbnail: "https://placehold.jp/800x600.png",
+//     projectDate: "2023-04-22",
+//     tags: ["Python", "AI", "NLP"],
+//     publicLink: "https://example.com/project10",
+//   },
+// ];
 
 type Status = "pending" | "fulfilled" | "rejected";
 
@@ -145,8 +145,18 @@ async function getData(url: string): Promise<any> {
   }
 }
 
-async function getProjects(): Promise<ProjectProps[]> {
+async function getProjects(): Promise<ProjectProps[] | unknown> {
   // Add a fake delay to make waiting noticeable.
-  await sleep(5000);
-  return dummyData;
+  try {
+    const response = await fetch("/projects");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const projects = await response.json();
+    return projects;
+  } catch (error) {
+    console.error(error ? error : "An unknown error occurred");
+    return error;
+  }
+  //await sleep(5000);
 }
