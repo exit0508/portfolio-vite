48428ffe495fde4615cf10a490ef4e961454bc1b
exit0508 <17ad060@ms.dendai.ac.jp>
2024-07-06 22:28:50 +0900

    Remove context

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ProjectProvider } from "./utils/projectContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ProjectProvider>
-      <App />
-    </ProjectProvider>
+    <App />
   </React.StrictMode>
 );
